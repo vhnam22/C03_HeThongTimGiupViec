5351a600fd00bab1cedfdf825d64811d1c64eb52
vhnam22 <113097282+vhnam22@users.noreply.github.com>
2024-06-05 21:22:08 +0700

    index home

--- C03_HeThongTimGiupViec/Repositories/Interface/IServicesRepository.cs
+++ C03_HeThongTimGiupViec/Repositories/Interface/IServicesRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace C03_HeThongTimGiupViec.Repository.Interface
+﻿using C03_HeThongTimGiupViec.Models;
+
+namespace C03_HeThongTimGiupViec.Repository.Interface
 {
     public interface IServicesRepository
     {
+        //Get all services
+        public List<Service> GetServices();
+
+        //Add new service
+        public bool AddService(Service service);
+
+        //Update service info
+        public bool UpdateService(Service service);
+
+
     }
 }
